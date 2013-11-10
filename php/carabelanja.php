efb05c9198c718e36b1c505bcba4d3b18841ee27
22104985 <vianbastio@gmail.com>
2013-11-10 11:42:33 +0700

    Create carabelanja.php

---
+++ php/carabelanja.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cara Belanja</title>
+<link rel="stylesheet" type="text/css" href="../css/as.css" />
+
+<link rel="stylesheet" href="../css/style.css" />
+
+    <script type="text/javascript" src="../js/jquery.js"></script>
+    <script type="text/javascript" src="../js/dropdown.js"></script>
+    <script type="text/javascript" src="../js/slides.js"></script>
+    <script type="text/javascript" src="../js/slideshow.js"></script>
+    <script type="text/javascript" src="../js/slide-menu.js"></script>
+      <script type="text/javascript">
+        $(document).ready(function()
+        {
+          
+
+          
+      $("a#login").click(function()
+          {
+            alert("hallo, silahkan masuk ");
+            
+          });
+      $("a#scart").click(function()
+          {
+            alert("masih kosong! ");
+            
+          });
+        });
+      </script>
+
+</head>
+<body>
+    <div id="wrapper">
+        <div id="headerwrap">
+        <div id="header"><img src="../images/logo.png" width="511" height="80">
+            <p id="trblock">
+              <a href="" id="scart">Keranjang Belanja</a><br>
+                       <a href="login_nodatabase.php" id="login" >Masuk  </a>
+                    <a href="akunbaru.php">  Daftar</a>
+          </p>
+        </div>
+        </div>
+        <div id="navigationwrap">
+        <div class="mn-container" >
+  
+    <ul id="topnav" >
+
+     <li><a href="index.php">Beranda</a></li>
+      <li><a href="carabelanja.php">Cara Belanja</a></li>
+      <li><a href="tentangkami.php">Tentang Kami</a></li>
+      
+    </ul>
+  </div>
+        </div>
+        <div id="leftcolumnwrap">
+        <div id="leftcolumn">
+          <table><thead>
+            <tr>
+            <th height="33" bgcolor="#00FFFF">Kategori</th>
+
+            </tr>
+
+
+            </thead>
+              <tbody>
+<tr>
+<td><a href="fd.php">Flashdisk</a></td>
+</tr>
+<tr>
+<td><a href="hdd.php">HD Eksternal</a></td>
+</tr>
+<tr>
+<td><a href="headphone.php">Headphone</a></td>
+</tr>
+<tr>
+<td><a href="modem.php">Modem</a></td>
+</tr>
+<tr>
+<td><a href="speaker.php">Speaker</a></td>
+</tr>
+
+            </tbody>
+
+
+
+
+
+
+            </table>
+            
+        </div>
+        </div>
+        <div id="contentwrap">
+        <div id="content1">
+          Cara Belanja
+          bla bla
+        </div>
+        </div>
+        <div id="rightcolumnwrap">
+        <div id="rightcolumn">
+          <table>
+            <thead>
+              <tr>
+                <th height="33" bgcolor="#00FFFF">Kontak Kami</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td height="200"><strong>Telepon</strong><br>
+                  0274-556677<br>
+                  <strong>Sms</strong><br>
+                  0852-5000-0000<br>
+                  <strong>Pin BB</strong><br>
+                  29A11110<br>
+                  <strong>Whatsapp / Sms</strong><br>
+                  0852-5000-0000<br>
+                  <br>
+                Jl Demangan Yogyakarta<p><img src="../images/facebook.png" width="60" height="60"><img src="../images/twitter.png" width="60" height="60"></p></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        </div>
+        <div id="footerwrap">
+        <div id="footer">
+            <p>Copyright © 2013 Dracones - Cepat & Murah Hanya Untuk Anda.</p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
