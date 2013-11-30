6f200e1bff5d3757f3b74136685fab61c05690c9
22104985 <vianbastio@gmail.com>
2013-11-30 18:40:16 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$currency = 'Rp. '; //Currency sumbol or code
+
+$db_username = 'root';
+$db_password = '';
+$db_name = 'mysql_import';
+$db_host = 'localhost';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
